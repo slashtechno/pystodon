0f2f0b90037fbccb5e2cab06a3bcea11f05d246b
slashtechno <77907286+slashtechno@users.noreply.github.com>
2023-08-27 13:25:45 -0500

    Fixed how the stream is run

--- disagreeable_posts/__main__.py
+++ disagreeable_posts/__main__.py
@@ -6,6 +6,7 @@ import trio
 from pathlib import Path
 import os
 from sys import stderr
+import datetime
 
 def main():
     load_dotenv(dotenv_path=Path('.') / '.env')
@@ -27,33 +28,33 @@ def main():
     # Testing hashtags now
     mastodon.status_post("Hello, Mastodon API! #test", idempotency_key='test-hashtag') # noqa E501
 
-    # Stream public timeline asynchronously
-    # logger.info("Starting public stream")
-    # public = mastodon.stream_public(TheStreamListener())
-    # logger.info("Started public stream")
-    # # Now we need to run the stream
-    # try:
-    #     trio.run(public.stream, trio_token="public")
-    # except KeyboardInterrupt:
-    #     logger.info("Stopping public stream")
-    #     public.close()
-    #     logger.info("Stopped public stream")
-    # Stream user timeline asynchronously
+   
     logger.info("Starting user stream")
-    user = mastodon.stream_user(TheStreamListener())
+    stream = mastodon.stream_user(TheStreamListener(), run_async=True)
     logger.info("Started user stream")
-    # Now we need to run the stream
+    trio.run(sleep_or_not, stream)
+
+
+async def sleep_or_not(stream):
+    # Experimenting with running other code while the stream is running
     try:
-        trio.run(user.stream, trio_token="user")
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(trio.sleep_forever)
+            nursery.start_soon(print_time)
     except KeyboardInterrupt:
         logger.info("Stopping user stream")
-        user.close()
-        logger.info("Stopped user stream")
+        # Stop the stream, but exit immediately if control c is hit again
+
+        
+async def print_time():
+        time = datetime.datetime.now().strftime("%H:%M:%S")
+        logger.info(f"The time is now: {time}")
+
 
 class TheStreamListener(StreamListener):
     def on_update(self, status):
         # An uodate is a post made by you
-        logger.info(f"Got update: {status['content']} of type {status['type']}")  # noqa E501
+        logger.info(f"Got update: {status['content']}")  # noqa E501
         
         # `default` is a parameter which sets a function to be called for objects that can't otherwise be serialized.  # noqa E501
         # In this case, it will call str(x) for any object x that it can't serialize.  # noqa E501
