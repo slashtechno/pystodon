2a2924ea75e6a58c94a975a85faf7ec85da5d9cf
slashtechno <77907286+slashtechno@users.noreply.github.com>
2023-08-29 13:33:23 -0500

    Delete replies when exiting

--- disagreeable_posts/__main__.py
+++ disagreeable_posts/__main__.py
@@ -1,4 +1,3 @@
-import json
 from loguru import logger
 from mastodon import Mastodon, StreamListener
 from dotenv import load_dotenv
@@ -8,30 +7,30 @@ import os
 from sys import stderr
 import datetime
 
+# Global variables
 mastodon = Mastodon
+delete_when_done = []
 
 def main():
     global mastodon
-    load_dotenv(dotenv_path=Path('.') / '.env')
+    load_dotenv(dotenv_path=Path(".") / ".env")
     access_token = os.getenv("MASTODON_ACCESS_TOKEN")
     api_base_url = os.getenv("MASTODON_API_BASE_URL")
 
     set_primary_logger("DEBUG")
 
-    # Now login 
-    mastodon = Mastodon(
-        access_token=access_token,
-        api_base_url=api_base_url
-    )
+    # Now login
+    mastodon = Mastodon(access_token=access_token, api_base_url=api_base_url)
     # Now show the username
     logger.info(f"Logged in as {mastodon.account_verify_credentials()['username']}")
     # Hello, Mastodon API!
-    mastodon.status_post("Hello, Mastodon API!", idempotency_key='test-hello')
+    mastodon.status_post("Hello, Mastodon API!", idempotency_key="test-hello")
     logger.info("Posted to Mastodon API")
     # Testing hashtags now
-    mastodon.status_post("Hello, Mastodon API! #test", idempotency_key='test-hashtag') # noqa E501
+    mastodon.status_post(
+        "Hello, Mastodon API! #test", idempotency_key="test-hashtag"
+    )  # noqa E501
 
-   
     logger.info("Starting user stream")
     stream = mastodon.stream_user(TheStreamListener(), run_async=True)
     logger.info("Started user stream")
@@ -46,34 +45,48 @@ async def sleep_or_not(stream):
             nursery.start_soon(print_time)
     except KeyboardInterrupt:
         logger.info("Stopping user stream")
-        # Stop the stream, but exit immediately if control c is hit again
+        # for status in delete_when_done:
+        #     mastodon.status_delete(status)
+        #     logger.info(f"Deleted {status}")
+    
 
-        
 async def print_time():
-        time = datetime.datetime.now().strftime("%H:%M:%S")
-        logger.info(f"The time is now: {time}")
+    time = datetime.datetime.now().strftime("%H:%M:%S")
+    logger.info(f"The time is now: {time}")
 
 
 class TheStreamListener(StreamListener):
     def on_update(self, status):
         # An uodate is a post made by you
         logger.info(f"Got update: {status['content']}")  # noqa E501
-        
+
         # `default` is a parameter which sets a function to be called for objects that can't otherwise be serialized.  # noqa E501
         # In this case, it will call str(x) for any object x that it can't serialize.  # noqa E501
         # logger.info(f"JSON: {json.dumps(status, indent=4, default=str)}")
 
         # https://docs.joinmastodon.org/entities/Status/
         # https://docs.joinmastodon.org/entities/Notification/
+
     def on_notification(self, notification):
-        if notification['type'] == 'mention':
-            logger.opt(colors=True).info(f"Got <blue>mention</blue> from {notification['account']['username']}") # noqa E501
-            mastodon.status_post(f"Hello, @{notification['account']['acct']}", in_reply_to_id=notification['status']['id'])
-            
-        elif notification['type'] == 'favourite':
-            logger.opt(colors=True).info(f"Got <yellow>favourite</yellow> from {notification['account']['username']}")  # noqa E501
-        else: 
-            logger.info(f"Got unknown notification: {notification['type']} from {notification['account']['username']}")  # noqa E501
+        if notification["type"] == "mention":
+            logger.opt(colors=True).info(
+                f"Got <blue>mention</blue> from {notification['account']['username']}"
+            )
+            post = mastodon.status_post(
+                f"Hello, @{notification['account']['acct']}",
+                in_reply_to_id=notification["status"]["id"],
+            )
+            delete_when_done.append(post["id"])
+
+
+        elif notification["type"] == "favourite":
+            logger.opt(colors=True).info(
+                f"Got <yellow>favourite</yellow> from {notification['account']['username']}"  # noqa E501
+            )
+        else:
+            logger.info(
+                f"Got unknown notification: {notification['type']} from {notification['account']['username']}"  # noqa E501
+            )
         logger.info(f"Content: {notification['status']['content']}")
         logger.info(f"ID: {notification['status']['id']}")
         logger.info(f"URL: {notification['status']['url']}")
@@ -81,13 +94,13 @@ class TheStreamListener(StreamListener):
         logger.info(f"Sensitive: {notification['status']['sensitive']}")
 
 
-
 def set_primary_logger(log_level):
     global logger
-    logger.remove() 
+    logger.remove()
     # ^10 is a formatting directive to center witzh a padding of 10
     logger_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> |<level>{level: ^10}</level>| <level>{message}</level>"  # noqa E501
     logger.add(stderr, format=logger_format, colorize=True, level=log_level)
     # logger = logger.opt(ansi=True)# noqa F841
+
 
 main()
