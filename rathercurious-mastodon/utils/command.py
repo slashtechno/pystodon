df4911b3210b3fe8dee6a9b490027de01c017afd
slashtechno <77907286+slashtechno@users.noreply.github.com>
2023-10-29 09:43:32 -0500

    Add function in `utils.py` to manage stream

--- rathercurious-mastodon/utils/command.py
+++ rathercurious-mastodon/utils/command.py
@@ -1,145 +1,156 @@
 import re
 from . import utils
 
+#
 
 
 class Command:
-    '''
+    """
     Functions and stuff related to adding and commands to Mastodon.py
     Intended to parse commands such as "@<bot> #<command> <arguments>".
-    '''
-    def __init__(self, hashtag, function: callable, help_arguments: dict = {}, *args, **kwargs): # noqa E501
+    """
+
+    def __init__(
+        self, hashtag, function: callable, help_arguments: dict = {}, *args, **kwargs
+    ):  # noqa E501
         self.hashtag = hashtag
         self.function = function
         self.function_args = args
         self.function_kwargs = kwargs
         self.help_arguments = help_arguments
-    
+
     # Setters/Getters
     @property
     def hashtag(self):
-        '''Get the hashtag'''
+        """Get the hashtag"""
         return self._hashtag
+
     @hashtag.setter
     def hashtag(self, hashtag):
-        '''Set the hashtag if it is just word characters. Does not include the #'''
-        if re.search(r'^\w+$', hashtag):
+        """Set the hashtag if it is just word characters. Does not include the #"""
+        if re.search(r"^\w+$", hashtag):
             self._hashtag = hashtag
         else:
-            raise ValueError('Hashtag must match regex: ^\w+$')
+            raise ValueError("Hashtag must match regex: ^\w+$")
 
     @property
     def function(self):
-        '''Get the function'''
+        """Get the function"""
         return self._function
+
     @function.setter
     def function(self, function):
-        '''
+        """
         Set the function if it is callable.
-        This function will be passed one argument, the complete status object   
+        This function will be passed one argument, the complete status object
         This function should return a string to be posted as a reply
-        '''
+        """
         if callable(function):
             self._function = function
         else:
-            raise TypeError('Function must be callable')
-    
+            raise TypeError("Function must be callable")
+
     @property
     def function_args(self):
-        '''Get the function arguments'''
+        """Get the function arguments"""
         return self._function_args
+
     @function_args.setter
     def function_args(self, function_args):
-        '''Set the function arguments'''
+        """Set the function arguments"""
         self._function_args = function_args
+
     @property
     def function_kwargs(self):
-        '''Get the function keyword arguments'''
+        """Get the function keyword arguments"""
         return self._function_kwargs
+
     @function_kwargs.setter
     def function_kwargs(self, function_kwargs):
-        '''Set the function keyword arguments'''
+        """Set the function keyword arguments"""
         self._function_kwargs = function_kwargs
-
-
 
     @property
     def help_arguments(self):
-        '''Get the arguments'''
+        """Get the arguments"""
         return self._help_arguments
+
     @help_arguments.setter
     def help_arguments(self, help_arguments):
-        '''
+        """
         Set arguments and their help text.
         This is used for the help command
 
         Please note, these arguments are not passed to the function, for that, use *args and **kwargs # noqa E501
-        '''
+        """
         if isinstance(help_arguments, dict):
             self._help_arguments = help_arguments
         else:
-            raise TypeError('Arguments must be a dictionary')
-    
+            raise TypeError("Arguments must be a dictionary")
+
     # Methods and stuff
     def __str__(self):
-        return self.hashtag 
-
+        return self.hashtag
 
     # class variables
+    # TODO: Prepend _ 
     commands = []
 
     # classmethods
 
     @classmethod
-    def add_command(cls, command: 'Command'):
-        '''
+    def add_command(cls, command: "Command"):
+        """
         Add a command to the list of commands.
-        '''
+        """
         if isinstance(command, Command) and command not in cls.commands:
             cls.commands.append(command)
         else:
-            raise TypeError('Argument must be a Command')
+            raise TypeError("Argument must be a Command")
 
     @classmethod
-    def delete_command(cls, command: 'Command'):
-        '''
+    def delete_command(cls, command: "Command"):
+        """
         Delete a command from the list of commands.
-        '''
+        """
         if isinstance(command, Command):
             cls.commands.remove(command)
         else:
-            raise TypeError('Argument must be a Command')
-    
+            raise TypeError("Argument must be a Command")
+
     @staticmethod
     def parse_status(status: dict, always_mention: bool, commands: list = commands):
-        '''
+        """
         Parse the status dict and call the appropriate command
         It passes the status dict, as well as any args and kwargs.
         Please note that the status dict is passed as the first argument.
-        
+
+        You can provide a custom list of commands, but if you don't, it will use the class variable
+        The class variable is updated with Command.add_command() and Command.delete_command() # noqa E501
+
         If the account sends a DM with the command, the bot will send a DM back.
         But if there isn't a mention, the account won't be able to see it
         Setting always_mention will prepend the content with "@author" and a newline
 
-        If no command matches, return None. 
-        '''
+        If no command matches, return None.
+        """
 
-        if commands == []:
-            return None 
-        
+        if commands is None:
+            commands = Command.commands
+
         # Get the hashtag
-        if matches := re.search(r'#(\w+)', utils.parse_html(status['content'])):
-                 hashtag = matches.group(1)
+        if matches := re.search(r"#(\w+)", utils.parse_html(status["content"])):
+            hashtag = matches.group(1)
         else:
             return None
-        
-    #    Run the command
-        if hashtag == 'help':
-            content = 'Commands:\n'
+
+        #    Run the command
+        if hashtag == "help":
+            content = "Commands:\n"
             for command in commands:
-                content += f'\n#{command.hashtag}\n'
+                content += f"\n#{command.hashtag}\n"
                 for argument, help_text in command.help_arguments.items():
-                    content += f'#{command.hashtag} {argument}: {help_text}\n'
+                    content += f"#{command.hashtag} {argument}: {help_text}\n"
             if always_mention:
                 # The Mastodon client Elk will seemingly not show the mention if it's on the first like  # noqa: E501
                 return f"@{status['account']['acct']}\n{content}"
@@ -149,7 +160,9 @@ class Command:
             for command in commands:
                 if hashtag == command.hashtag:
                     # "*" unpacks the list of arguments, while "**" unpacks the dictionary of keyword arguments # noqa E501
-                    content = command.function(status, *command.function_args, **command.function_kwargs)
+                    content = command.function(
+                        status, *command.function_args, **command.function_kwargs
+                    )
                     if always_mention:
                         # The Mastodon client Elk will seemingly not show the mention if it's on the first like  # noqa: E501
                         return f"@{status['account']['acct']}\n{content}"
--- rathercurious-mastodon/utils/utils.py
+++ rathercurious-mastodon/utils/utils.py
@@ -1,32 +1,136 @@
 from bs4 import BeautifulSoup
 import re
+from mastodon import Mastodon, StreamListener
+from .command import Command
+import trio
+
+class stream_listener:
+    """
+    A class of functions to allow for easy streaming of Mastodon statuses with sensible defaults.
+    """
+
+    def __init__(
+        self,
+        mastodon_access_token: str,
+        mastodon_api_base_url: str,
+        delete_when_done: bool = False,
+        always_mention: bool = True,
+        commands: list = None,
+    ):
+        """
+        Initialize the class.
+        """
+        # self.mastodon_access_token = mastodon_access_token
+        # self.mastodon_api_base_url = mastodon_api_base_url
+        self.delete_when_done = delete_when_done
+        self.always_mention = always_mention
+        self.commands = commands
+
+        self.mastodon = Mastodon(
+            access_token=mastodon_access_token, api_base_url=mastodon_api_base_url
+        )
+
+    class partially_configured_stream_listener(StreamListener):
+        """
+        What events cause what actions.
+        """
+
+        def __init__(
+            self,
+            mastodon: Mastodon,
+            delete_when_done: bool = False,
+            always_mention: bool = True,
+            commands: list = None,
+        ):
+            self.mastodon = mastodon
+            self.always_mention = always_mention
+            self.commands = commands
+            self.posts_to_delete = []
+
+        def on_update(self, status):
+            # As far as I can tell, an update caused when you reblog or when an account you follow posts something  # noqa E501
+            # logger.info(f"JSON: {json.dumps(status, indent=4, default=str)}")
+            # https://docs.joinmastodon.org/entities/Status/
+            # https://docs.joinmastodon.org/entities/Notification/
+            pass
+
+        def on_notification(self, notification):
+            if notification["type"] == "mention":
+                content = Command.parse_status(
+                    status=notification["status"],
+                    always_mention=self.always_mention,
+                    commands=self.commands,
+                )  # noqa E501
+                post = self.mastodon.status_post(
+                    # Set the content of the status to the string returned above
+                    content,
+                    # Reply to the mention
+                    in_reply_to_id=notification["status"]["id"],
+                    # Match the visibility of the mention
+                    visibility=notification["status"]["visibility"],
+                )
+                self.posts_to_delete.append(post["id"])
+
+            elif notification["type"] == "favourite":
+                pass
+            else:
+                pass
+    def stream(self):
+        """
+        Stream statuses.
+        """
+        self.fully_configured_stream_listener = self.partially_configured_stream_listener(
+            mastodon=self.mastodon,
+            delete_when_done=self.delete_when_done,
+            always_mention=self.always_mention,
+            commands=self.commands,
+        )
+        self.mastodon.stream_user(self.fully_configured_stream_listener, run_async=True)
+        trio.run(self.sleep_or_not)
+
+    async def sleep_or_not(self):
+        '''Used to optionally run other code while the stream is running, in addition to optionally deleting posts when done'''
+        try:
+            async with trio.open_nursery() as nursery:
+                nursery.start_soon(trio.sleep_forever)
+                nursery.start_soon(self.need_to_run)
+        except KeyboardInterrupt:
+            if self.delete_when_done:
+                for post in self.fully_configured_stream_listener.posts_to_delete:
+                    self.mastodon.status_delete(post)
+
+    @staticmethod
+    async def need_to_run():
+        '''Code that needs to run while the stream is running'''
+        pass
 
 def return_raw_argument(status: dict):
-    '''
-    Return the raw arguments (everything after the hashtag) as a string. 
+    """
+    Return the raw arguments (everything after the hashtag) as a string.
     Uses utils.parse_html() to parse the HTML, adding newlines after every <p> tag
     In many cases, if regex is being used anyway, it's better to use that instead
-    '''
-    content = parse_html(html_content=status['content'])
+    """
+    content = parse_html(html_content=status["content"])
     # Match from the beginning of the string, a hashtag, a space, and then ANYTHING, including newlines # noqa E501
     # re.DOTALL is present so commands can span multiple lines
     if matches := re.search(
-        r'^(?:@\w+\s+)(?:#\w+\s+)(.+)$', 
-        # TODO: mke mention optional as the "@<account" will not be present if it's a reply
-        content, 
-        flags=re.IGNORECASE | re.DOTALL
-        ):
+        r"^(?:@\w+\s+)(?:#\w+\s+)(.+)$",
+        # From what I can tell, there will always be a mention if it's a reply. Some clients just don't show it in the body content
+        content,
+        flags=re.IGNORECASE | re.DOTALL,
+    ):
         return matches.group(1)
     else:
         # Unsure if it should return None or an empty string
         return None
 
-def parse_html(html_content:str):
-    '''Return the raw post content, with newlines after every <p> tag'''
 
-    content = BeautifulSoup(html_content, 'html.parser')
-    for p in content.find_all('p'):
-        p.insert_after('\n')
+def parse_html(html_content: str):
+    """Return the raw post content, with newlines after every <p> tag"""
+
+    content = BeautifulSoup(html_content, "html.parser")
+    for p in content.find_all("p"):
+        p.insert_after("\n")
         # Perhaps don't insert a newline if it's the last <p> tag?
-    raw_content = content.get_text(separator='')
+    raw_content = content.get_text(separator="")
     return raw_content
